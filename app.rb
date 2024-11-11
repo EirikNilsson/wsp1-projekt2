05b4150c942fbe2980e641c341298f120eedd60b
EirikNilsson <eirik.haugennilsson@elev.ga.ntig.se>
2024-11-11 14:58:22 +0100

    Added remove/add/change

--- app.rb
+++ app.rb
@@ -1,7 +1,52 @@
+require 'sinatra'
+require 'sqlite3'
+
 class App < Sinatra::Base
+  def db_connection
+    db = SQLite3::Database.new 'db/Todo.sqlite'
+    db.results_as_hash = true
+    db
+  end
 
-    get '/' do
-        erb(:"index")
-    end
+  # Visa alla uppgifter
+  get '/' do
+    db = db_connection
+    @Todo = db.execute("SELECT * FROM Todo ORDER BY impScale ASC")
+    erb :index
+  end
 
+  # Form för att skapa en ny uppgift
+  get '/thing/new' do
+    erb :new
+  end
+
+  # Skapa en ny uppgift i databasen
+  post '/thing' do
+    db = db_connection
+    db.execute("INSERT INTO Todo (thing, impScale, description) VALUES (?, ?, ?)", 
+    [params[:thing], params[:impScale], params[:description]])
+    redirect '/'
+  end
+
+  # Visa en uppgift för redigering
+  get '/thing/:id/edit' do
+    db = db_connection
+    @thing = db.execute("SELECT * FROM Todo WHERE id = ?", params[:id]).first
+    erb :edit
+  end
+
+  # Uppdatera en uppgift
+  post '/thing/:id' do
+    db = db_connection
+    db.execute("UPDATE Todo SET thing = ?, impScale = ?, description = ? WHERE id = ?", 
+    [params[:thing], params[:impScale], params[:description]])
+    redirect '/'
+  end
+
+  # Ta bort en uppgift
+  post '/thing/:id/delete' do
+    db = db_connection
+    db.execute("DELETE FROM Todo WHERE id = ?", params[:id])
+    redirect '/'
+  end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,11 +1,36 @@
 require 'sqlite3'
 
 class Seeder
-
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS Todo')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE Todo (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  thing TEXT NOT NULL,
+                  impScale INTEGER,
+                  description TEXT
+                )')
+  end
+
+  def self.populate_tables
+    db.execute('INSERT INTO Todo (thing, impScale, description) VALUES (?, ?, ?)', "Äta mat", 1, "Måste äta mat för att må bra")
+  end
+
+  private
+
+  def self.db
+    @db ||= SQLite3::Database.new('db/Todo.sqlite').tap do |db|
+      db.results_as_hash = true
+    end
+  end
 end
 
 Seeder.seed!
